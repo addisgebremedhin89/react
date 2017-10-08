6155f69c378a336edf86103917a41e73ad261ca8
Tehut <tehut.m.g@gmail.com>
2017-10-08 14:42:25 -0700

    configured babel

---
+++ indecisonapp/src/app.js
@@ -0,0 +1,9 @@
+console.log("App.js is running!");
+
+var template =<h1>Indecision App</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+//this is our very own application, we are accessig the reactdom library and applying the method render that will take in two arguments
+//reactdoms method we will be using is .render
+
